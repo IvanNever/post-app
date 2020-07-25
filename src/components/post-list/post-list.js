c9447204fa6c2f3142fe2e98f91b3dc8d5dc2c24
Ivan Neverovsky <ivan@neverovsky.com.ua>
2020-07-25 13:03:23 +0300

    Component properties and states

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -3,12 +3,21 @@ import PostListItem from '../post-list-item';
 
 import './post-list.css';
 
-const PostList = () => {
+const PostList = ({posts}) => {
+
+    const elements = posts.map((elem) => {
+        const {id, ...itemProps} = elem;
+
+        return (
+            <li key={id} className='list-group-item'>
+                <PostListItem {...itemProps}/>
+            </li>
+        )
+    });
+
     return (
         <ul className="app-list list-group">
-            <PostListItem/>
-            <PostListItem/>
-            <PostListItem/>
+            {elements}
         </ul>
     )
 }
--- src/components/post-status-filter/post-status-filter.js
+++ src/components/post-status-filter/post-status-filter.js
@@ -4,9 +4,9 @@ import './post-status-filter.css';
 
 const PostStatusFilter = () => {
     return (
-        <div className="btn-group">
-            <button/>
-            <button/>
+        <div className='btn-group'>
+            <button type='button' className='btn btn-info'>All</button>
+            <button type='button' className='btn btn-outline-secondary'>Liked</button>
         </div>
     )
 }
